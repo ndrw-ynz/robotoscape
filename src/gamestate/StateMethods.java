67aeda7cb8aa7534bacf2b038005754b0f72fbde
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-02 18:35:18 +0800

    Removed unnecessary void

--- src/gamestate/StateMethods.java
+++ src/gamestate/StateMethods.java
@@ -14,46 +14,46 @@ public interface StateMethods {
      * on the game screen.
      * @param graphics The graphics object that draws state components on the game screen.
      */
-    public void render(Graphics graphics);
+    void render(Graphics graphics);
 
     /**
      * update | Updates the states of the game.
      */
-    public void update();
+    void update();
 
     /**
      * mouseClicked | Executes events following the click of the user's mouse.
      * @param e A MouseEvent containing information about user mouse input.
      */
-    public void mouseClicked(MouseEvent e);
+    void mouseClicked(MouseEvent e);
 
     /**
      * mousePressed | Executes events following the pressed buttons of the user's mouse.
      * @param e A MouseEvent containing information about user mouse input.
      */
-    public void mousePressed(MouseEvent e);
+    void mousePressed(MouseEvent e);
 
     /**
      * mouseReleased | Executes events following the release of the buttons of the user's mouse.
      * @param e A MouseEvent containing information about user mouse input.
      */
-    public void mouseReleased(MouseEvent e);
+    void mouseReleased(MouseEvent e);
 
     /**
      * mouseMoved | Executes events following the movement of the user's mouse.
      * @param e A MouseEvent containing information about user mouse input.
      */
-    public void mouseMoved(MouseEvent e);
+    void mouseMoved(MouseEvent e);
 
     /**
      * keyPressed | Executes events following the press of the keys of the user's keyboard.
      * @param e A KeyEvent containing information about user keyboard input.
      */
-    public void keyPressed(KeyEvent e);
+    void keyPressed(KeyEvent e);
 
     /**
      * keyReleased | Executes events following the release of the keys of the user's keyboard.
      * @param e A KeyEvent containing information about user keyboard input.
      */
-    public void keyReleased(KeyEvent e);
+    void keyReleased(KeyEvent e);
 }
