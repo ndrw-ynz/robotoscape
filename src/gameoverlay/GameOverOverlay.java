aa58b55c989c7c6b145191f1ee0b799e6c53b052
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-03 21:02:16 +0800

    Finished GameOverOverlay for display of game over screen.

--- src/gameoverlay/GameOverOverlay.java
+++ src/gameoverlay/GameOverOverlay.java
@@ -1,10 +1,15 @@
 package gameoverlay;
 
+import gamestate.GameState;
+import gamestate.PlayState;
 import main.Game;
 import text.InteractiveText;
 import text.RegularText;
+import utility.Atlas;
 
 import java.awt.*;
+
+import static utility.TextUtils.isWithinBoundary;
 
 /**
  * The GameOverOverlay class extends the Overlay class.
@@ -12,8 +17,10 @@ import java.awt.*;
  */
 public class GameOverOverlay extends Overlay{
 
-//    private final RegularText gameOverText;
-//    private final InteractiveText exitText;
+    /**The game over text of the game over overlay.*/
+    private final RegularText gameOverText;
+    /**The interactive exit text of the game over overlay.*/
+    private final InteractiveText exitText;
 
     /**
      * GameOverOverlay instantiates an overlay that displays the game over overlay of the game.
@@ -21,24 +28,28 @@ public class GameOverOverlay extends Overlay{
      * @param windowWidth   The width of the overlay window in pixels.
      * @param windowHeight  The height of the overlay window in pixels.
      */
-    public GameOverOverlay(Game game, int windowWidth, int windowHeight) {
-        super(game, windowWidth, windowHeight);
-//        this.gameOverText = new RegularText();
-//        this.exitText = new InteractiveText();
+    public GameOverOverlay(Game game, PlayState playState, int windowWidth, int windowHeight) {
+        super(game, playState, windowWidth, windowHeight);
+        this.gameOverText = new RegularText(368, "Game Over", 50, 200, Atlas.TARRGET_FONT);
+        this.exitText = new InteractiveText(428, "Exit", 40, 200, Atlas.TARRGET_FONT);
     }
 
     @Override
     protected void drawOverlayText(Graphics2D graphics) {
-
+        gameOverText.renderText(graphics, game.getScreenWidth());
+        exitText.renderText(graphics, game.getScreenWidth());
     }
 
     @Override
     public void updateInteractiveText(int x, int y) {
-
+        exitText.setIsActive(isWithinBoundary(x, y, exitText.getBoundaryBox()));
     }
 
     @Override
     public void updateState() {
-
+        if (exitText.isActive()) {
+            GameState.state = GameState.MENU;
+            game.resetPlayState();
+        }
     }
 }
